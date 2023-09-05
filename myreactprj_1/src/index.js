46e11d5ca8d554d9333d1c8922502fb7f8ba901d
Lainari <108247620+Lainari@users.noreply.github.com>
2023-09-05 20:53:03 +0900

    Test

--- myreactprj_1/src/index.js
+++ myreactprj_1/src/index.js
@@ -8,7 +8,6 @@ const root = ReactDOM.createRoot(document.querySelector('#root'));
 root.render(
   <>
     <App/>
-    <App></App>
     {/* <App></App> 위에거랑 같은 효과*/} {/*주석처리~*/}
   </>
   // <> ... </> 안에 있는 컨포넌트 내용을 묶을 수 있는 장점으로 만들 수 있다!
